2b544ad6d0ea9bb048877da9f72419529824e18c
Harshida <harshidayapally@gmail.com>
2025-04-27 13:01:01 +0530

    base url modified

--- frontend/ticket-booking/src/axios/axiosInstance.js
+++ frontend/ticket-booking/src/axios/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const userInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
 
